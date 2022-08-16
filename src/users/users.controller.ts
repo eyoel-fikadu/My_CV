aca17f805b79196f77688b005edc97611c07cfde
Eyoel_Fikadu <se.eyoel.fikadu@gmail.com>
2022-08-16 22:44:10 +0300

    add hooks and save user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-users.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
   @Post('signup')
   createUser(@Body() user: CreateUserDto) {
-    console.log(user);
+    this.userService.create(user.email, user.password);
   }
 }
