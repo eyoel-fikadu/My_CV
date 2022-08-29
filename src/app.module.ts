0aca452dbbd954343d837483f37e3d9801c2e17c
Eyoel_Fikadu <se.eyoel.fikadu@gmail.com>
2022-08-29 11:23:22 +0300

    add dotenv but not working

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,34 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        console.log('Database =>', config.get('DB_NAME'));
+        console.log('Config =>', config);
+
+        return {
+          type: 'sqlite',
+          database: 'db.sqlite',
+          synchronize: true,
+          entities: [User, Report],
+        };
+      },
     }),
     UsersModule,
     ReportsModule,
